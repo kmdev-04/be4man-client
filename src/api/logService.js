7bd0dd0a290460615d464063037a39e6b3d9e585
kmdev-04 <kmdev0494@gmail.com>
2025-10-27 09:09:48 +0000

    chore BM-28 file delete

--- src/api/logService.js
+++
@@ -1,9 +0,0 @@
-// src/api/logService.js
-import { API_ENDPOINTS } from '@/config/api';
-
-import axiosInstance from './axios';
-
-export const getLogs = async () => {
-  const response = await axiosInstance.get(API_ENDPOINTS.LOGS); // '/api/history' 사용
-  return response.data.content; // ✅ Page 객체의 content 배열 반환
-};
